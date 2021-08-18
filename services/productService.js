c9d94ff540eb3aaf894a759c4a1ed74fe04568a2
Gaurav Khodwe <GauravKhodwe@Gauravs-MBP.attlocal.net>
2021-08-18 14:48:04 -0500

    update reference

--- services/productService.js
+++ services/productService.js
@@ -1,4 +1,4 @@
-const Product = require('../Database/models/productModel');
+const Product = require('../database/models/productModel');
 const DBUtil = require('../utils/DBUtil');
 const constant = require('../constants');
 
